aaa1cb4c19f78e99f5af88d5be69b9e633b9c313
behshad <82580731+biigii@users.noreply.github.com>
2023-05-18 21:02:25 +1200

    javascript

---
+++ main.js
@@ -0,0 +1,56 @@
+// alert("hello world");
+
+// string
+// console.log('hello world');
+
+// numbers
+// console.log(2000)
+// console.log(1000 + 2000);
+// console.log('hello ' + 'world');
+
+// variable
+// camel casing
+var myAwesomeVariable;
+var numberOne = 100;
+var userFirstname = "john";
+// background-color ===> backgroundColor
+// grid-auto-column ===> gridAutoColumn
+
+var number1 = 2000;
+number1 = 5000;
+number1 = 5000 + 7000;
+// console.log(number1);
+
+var num1 = 30;
+var num2 = 100;
+var sum = num1 + num2;
+// console.log(sum);
+
+var value;
+value = 1000;
+value = 2000;
+
+// const and let, as well as var
+
+const value2 = 3;
+// value2 = 5; assignment is not allowed
+
+let value3;
+value3 = 9;
+value3 = 10;
+// console.log(value3);
+
+// boolean
+//  0 and 1 , on and off, true or false
+const isNight = true;
+const isSummer = false;
+
+// list or arrays
+const fruits = ["apple", "orange", "kiwi", "watermelon"];
+const myNumbersArray = [1, 89, 45, 1001];
+const mixedArray = ["hello world", 1, false, "123", 123];
+
+// console.log(100 + 100);
+// console.log("100" + "100");
+// console.log(100 + "100");
+document.body.style.backgroundColor = '#000'
